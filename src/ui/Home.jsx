c8cc7fd3ec248efe7e7c7412265b10b55ccfbbd6
Oleh <forProgIt@gmail.com>
2023-08-04 16:53:27 +0300

    Fixed and error in index.css

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8  text-xl font-semibold md:text-3xl ">
         The best cinnabons
-        <img className="inline h-[45px] w-[45px]" src="/cinnabon.png" alt="" />
+        <img className="inline h-[45px] w-[45px] " src="/cinnabon.png" alt="" />
         <br />
         <span className="text-sky-600">
           Straight out of the oven, straight to you.
